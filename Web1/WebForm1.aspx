9f43bf0b5e82110b6906910089a651b750959ab2
11vanshika <vanshikaa@moback.com>
2022-10-31 21:59:32 +0530

    Web Form

---
+++ Web1/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Web1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <table>
+                <tr><td>Student Name<asp:TextBox ID="StudentName" runat="server"></asp:TextBox></td> </tr>
+                <tr><td>Student Age<asp:TextBox ID="Studentage" runat="server"></asp:TextBox></td> </tr>
+                <tr><td>EmailId <asp:TextBox ID="emailId" runat="server"></asp:TextBox></td> </tr>
+                <tr><td>Password<asp:TextBox ID="password" runat="server"></asp:TextBox></td> </tr>
+                <tr><td>Contact Number<asp:TextBox ID="contactnumber" runat="server"></asp:TextBox></td> </tr>
+                <tr><td> Department<asp:TextBox ID="Department" runat="server"></asp:TextBox></td></tr>
+                <tr><td><asp:Button ID="Insert" runat="server" Text="Insert" OnClick="Insert_Click"/></td></tr>
+                <tr><td><asp:Button ID="Update" runat="server" Text="Update" OnClick="Update_Click"/></td></tr>
+                <tr><td><asp:Button ID="Delete" runat="server" Text="Delete" OnClick="Delete_Click"/></td></tr>
+                <tr><td><asp:Button ID="Read" runat="server" Text="Read" Onclick="Read_Click"/></td></tr>
+               <tr><td><asp:Label ID="Message" runat="server" Text="Message"></asp:Label></td></tr>
+
+
+
+               <tr><td><asp:GridView ID="GridView1" runat="server"></asp:GridView></td></tr>
+
+             </table>
+        </div>
+   </form>
+</body>
+</html>
---
+++ Web1/WebForm1.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Data;
+using System.Configuration;
+
+namespace Web1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+       SqlConnection sqlConnection = new SqlConnection("Data Source=.;Initial Catalog=student_Details;Integrated Security=True");
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+                string insertquery = "Exec Sp_InsertData '" + StudentName.Text + "','" + Studentage.Text + "','" + emailId.Text + "' , '" + password.Text + "','" + contactnumber.Text + "','" + Department.Text + "'";
+                SqlCommand cmd = new SqlCommand(insertquery, sqlConnection);
+               
+                cmd.ExecuteNonQuery();
+                Message.Text = "Data Inserted";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        //----------------Update Data------------------------//
+        protected void Update_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+                string updatequery = "SP_updateData'" + StudentName.Text + "','" + Studentage.Text + "','" + emailId.Text + "' , '" + password.Text + "','" + contactnumber.Text + "','" + Department.Text + "'";
+                SqlCommand cmd = new SqlCommand(updatequery, sqlConnection);
+                
+                cmd.ExecuteNonQuery();
+                Message.Text = "Data Updated";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        //---------------- Delete Data---------------------//
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string deletequery = "SP_deleteData'" + emailId.Text + "','" + password.Text + "'";
+                SqlCommand cmd = new SqlCommand(deletequery, sqlConnection);
+                sqlConnection.Open();
+                cmd.ExecuteNonQuery();
+                Message.Text = "Delete data";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+
+        //----------------Read Data------------------------
+        protected void Read_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string Readquery = " select * from [dbo].[Student_details_View];";
+                SqlCommand cmd = new SqlCommand(Readquery, sqlConnection);
+                sqlConnection.Open();
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                sqlDataAdapter.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                Message.Text = "Data Display";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
